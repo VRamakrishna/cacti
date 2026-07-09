2973eb432446f6b5c90c3d533c2c761bf00a8107
github-action-benchmark <github@users.noreply.github.com>
2026-07-09 11:10:59 +0000

    add Benchmark (benchmarkjs) benchmark result for 366055e57c984c9323990f605fda445297ada8ed

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783595458651,
+  "repoUrl": "https://github.com/VRamakrishna/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Rafael Belchior",
+            "username": "RafaelAPB",
+            "email": "rafael.belchior@tecnico.ulisboa.pt"
+          },
+          "committer": {
+            "name": "Rafael Belchior",
+            "username": "RafaelAPB",
+            "email": "RafaelAPB@users.noreply.github.com"
+          },
+          "id": "366055e57c984c9323990f605fda445297ada8ed",
+          "message": "refactor: remove keychain plugins\n\nAddresses #4025\nSigned-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>",
+          "timestamp": "2026-07-06T12:10:15Z",
+          "url": "https://github.com/VRamakrishna/cacti/commit/366055e57c984c9323990f605fda445297ada8ed"
+        },
+        "date": 1783595457128,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 662,
+            "range": "±2.72%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 663,
+            "range": "±2.90%",
+            "unit": "ops/sec",
+            "extra": "182 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
