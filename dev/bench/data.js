e24a3f5b377d8724de4c3e230c5dc256f5c2f204
github-action-benchmark <github@users.noreply.github.com>
2026-08-10 09:36:01 +0000

    add Benchmark (benchmarkjs) benchmark result for 9195eccc0a1b81112107e3e5be325c85efb95b72

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786013881133,
+  "lastUpdate": 1786354561095,
   "repoUrl": "https://github.com/VRamakrishna/cacti",
   "entries": {
     "Benchmark": [
@@ -554,6 +554,42 @@ window.BENCHMARK_DATA = {
             "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
             "value": 886,
             "range": "±4.51%",
+            "unit": "ops/sec",
+            "extra": "182 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "VRamakrishna",
+            "username": "VRamakrishna",
+            "email": "vramakr2@in.ibm.com"
+          },
+          "committer": {
+            "name": "Rafael Belchior",
+            "username": "RafaelAPB",
+            "email": "RafaelAPB@users.noreply.github.com"
+          },
+          "id": "9195eccc0a1b81112107e3e5be325c85efb95b72",
+          "message": "build(deps): updated vulnerable decompress package\n\nAddresses critical dependabot alert #3745.\nReplaced \"decompress\" npm package with \"@xhmikosr/decompress@11.1.3\".\n\nSigned-off-by: VRamakrishna <vramakr2@in.ibm.com>",
+          "timestamp": "2026-08-06T12:59:22Z",
+          "url": "https://github.com/VRamakrishna/cacti/commit/9195eccc0a1b81112107e3e5be325c85efb95b72"
+        },
+        "date": 1786354559229,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 590,
+            "range": "±3.61%",
+            "unit": "ops/sec",
+            "extra": "176 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 605,
+            "range": "±2.23%",
             "unit": "ops/sec",
             "extra": "182 samples"
           }
